d0ca0980e4ae60f572262f208e5dd207cfde61ff
nely de faria <nelly.nf54@gmail.com>
2020-08-17 18:08:42 -0300

    Define propriedades dinamicamente

--- php-oo-tw2/magicos/construtor.php
+++ php-oo-tw2/magicos/construtor.php
@@ -5,5 +5,9 @@ require_once '../autoload/autoload-psr4.php';
 $cli = new App\Classes\Cliente("Elton", "11 xxxxx-xxxx", 28);
 $cli->endereco = "Sp";
 
+$cli->cpf = "123.456.789-10";
+$cli->rg = "11.111.111.0";
+
+echo $cli->city;
 //unset($cli); destrói o objeto
 var_dump($cli);
