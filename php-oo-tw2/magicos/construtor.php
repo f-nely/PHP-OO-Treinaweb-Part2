7abc01b1ac32a58c87d6d7d9ba89b855ae73ece4
nely de faria <nelly.nf54@gmail.com>
2020-08-17 17:13:50 -0300

    Utiliza função unset

--- php-oo-tw2/magicos/construtor.php
+++ php-oo-tw2/magicos/construtor.php
@@ -4,4 +4,6 @@ require_once '../autoload/autoload-psr4.php';
 
 $cli = new App\Classes\Cliente("Elton", "11 xxxxx-xxxx", 28);
 $cli->endereco = "Sp";
+
+//unset($cli); destrói o objeto
 var_dump($cli);
