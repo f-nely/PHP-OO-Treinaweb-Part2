64873e99ab73955d5a64e2f02fd417c632fdf084
nely de faria <nelly.nf54@gmail.com>
2020-08-18 17:53:54 -0300

    Define atributo e método da classe

--- php-oo-tw2/src/Classes/Eletrodomesticos.php
+++ php-oo-tw2/src/Classes/Eletrodomesticos.php
@@ -6,5 +6,12 @@ namespace App\Classes;
 
 class Eletrodomesticos extends Produto
 {
+    public int $voltagem;
 
+    public function defineVoltagem(int $voltagem): void
+    {
+        if ($voltagem === 110 || $voltagem === 220) {
+            $this->voltagem = $voltagem;
+        }
+    }
 }
