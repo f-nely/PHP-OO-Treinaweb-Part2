8f4241aa8e3460d340fcb40c524233378230b681
nely de faria <nelly.nf54@gmail.com>
2020-08-18 18:14:05 -0300

    Instancia objeto da classe Microondas

--- php-oo-tw2/heranca/extensao.php
+++ php-oo-tw2/heranca/extensao.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Classes\Eletrodomesticos;
+use App\Classes\Microondas;
 
 require_once '../autoload/autoload-psr4.php';
 
@@ -10,4 +11,12 @@ $obj->titulo = "Geladeira";
 $obj->preco = 2200;
 $obj->defineCodigoBarras("00011");
 $obj->defineVoltagem(110);
-var_dump($obj);
+
+$obj2 = new Microondas();
+$obj2->titulo = "Microondas";
+$obj2->preco = 400;
+$obj2->defineCodigoBarras("00022");
+$obj2->defineVoltagem(220);
+$obj2->definePotencia(620);
+
+var_dump($obj, $obj2);
