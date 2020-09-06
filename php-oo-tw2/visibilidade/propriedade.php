05a830220ecd0b069c76f396def139b19016b043
nely de faria <nelly.nf54@gmail.com>
2020-09-06 17:21:33 -0300

    Define descricao para o objeto geladeira

--- php-oo-tw2/visibilidade/propriedade.php
+++ php-oo-tw2/visibilidade/propriedade.php
@@ -7,4 +7,5 @@ $prod = new App\Classes\Produto("Cerveja");
 
 $geladeira = new \App\Classes\Eletrodomesticos("geladeira", 110);
 $geladeira->definePreco(1400);
+$geladeira->descricao = "Modelo x2000";
 $geladeira->detalhes();
