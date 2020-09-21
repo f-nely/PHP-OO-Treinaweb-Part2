732d9fc05a2ba4f4436c660b8912e299e7da266a
nely de faria <nelly.nf54@gmail.com>
2020-09-21 19:26:45 -0300

    Adiciona classe Curso

---
+++ php-oo-tw2/desafios/Curso.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+class Curso
+{
+    private string $nome;
+    private string $descricao;
+
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): void
+    {
+        $this->nome = $nome;
+    }
+
+    public function getDescricao(): string
+    {
+        return $this->descricao;
+    }
+
+    public function setDescricao(string $descricao): void
+    {
+        $this->descricao = $descricao;
+    }
+
+
+}
