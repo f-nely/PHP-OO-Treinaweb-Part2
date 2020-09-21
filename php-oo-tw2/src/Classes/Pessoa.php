7884a868f7ecc2cf38564d014432b69141352a38
nely de faria <nelly.nf54@gmail.com>
2020-09-21 18:39:54 -0300

    Adiciona classe Pessoa

---
+++ php-oo-tw2/src/Classes/Pessoa.php
@@ -0,0 +1,60 @@
+<?php
+
+
+namespace App\Classes;
+
+
+abstract class Pessoa
+{
+    private string $nome;
+
+    private string $endereco;
+
+    private int $idade;
+
+    private string $telefone;
+
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): void
+    {
+        $this->nome = $nome;
+    }
+
+    public function getEndereco(): string
+    {
+        return $this->endereco;
+    }
+
+    public function setEndereco(string $endereco): void
+    {
+        $this->endereco = $endereco;
+    }
+
+    public function getIdade(): int
+    {
+        return $this->idade;
+    }
+
+    public function setIdade(int $idade): void
+    {
+        $this->idade = $idade;
+    }
+
+    public function getTelefone(): string
+    {
+        return $this->telefone;
+    }
+
+    public function setTelefone(string $telefone): void
+    {
+        $this->telefone = $telefone;
+    }
+
+
+
+}
