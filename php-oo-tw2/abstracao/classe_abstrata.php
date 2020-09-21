395001c847fbe717c0c4cf7ff20e1ed8e3e32d09
nely de faria <nelly.nf54@gmail.com>
2020-09-21 18:41:14 -0300

    Adiciona arquivo classe_abstrata.php

---
+++ php-oo-tw2/abstracao/classe_abstrata.php
@@ -0,0 +1,14 @@
+<?php
+
+
+require_once '../autoload/autoload-psr4.php';
+
+
+$cliente = new App\Classes\Cliente();
+
+$cliente->setNome("Elton");
+$cliente->setIdade(28);
+$cliente->setTelefone("11 - 00000-1111");
+$cliente->setEndereco("São Paulo");
+
+var_dump($cliente);
