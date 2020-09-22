b46d94bce0d2ccb77e6ceb47e132ddaa640ac558
nely de faria <nelly.nf54@gmail.com>
2020-09-22 17:38:41 -0300

    Adiciona arquivo interfaces.php

---
+++ php-oo-tw2/abstracao/interfaces.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once '../autoload/autoload-psr4.php';
+
+use App\Interfaces\Imprimivel;
+
+$produto = new App\Classes\Produto("Beer");
+$produto->defineCodigoBarras("00022");
+$produto->definePreco(10);
+
+$produto->detalhes();
