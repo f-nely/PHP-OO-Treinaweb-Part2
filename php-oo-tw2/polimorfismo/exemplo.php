e0b0cc8d51dedb2e5e0bd39fe1934fbc84ca2eae
nely de faria <nelly.nf54@gmail.com>
2020-09-24 17:46:54 -0300

    Instacia AdaptadorBase

--- php-oo-tw2/polimorfismo/exemplo.php
+++ php-oo-tw2/polimorfismo/exemplo.php
@@ -5,5 +5,6 @@ require_once '../autoload/autoload-psr4.php';
 $email = new App\Email\Envio();
 $mailgun = new App\Email\Adaptadores\Mailgun\Adaptador();
 $ses = new App\Email\Adaptadores\SES\Adaptador();
+$base = new \App\Email\Adaptadores\AdaptadorBase();
 
-$email->envia($ses);
+$email->envia($base);
