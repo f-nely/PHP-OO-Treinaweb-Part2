63e8ce68e53f10625d9298a5484241fe27c23af4
nely de faria <nelly.nf54@gmail.com>
2020-10-01 17:35:19 -0300

    Usando trait na classe Cliente

--- php-oo-tw2/src/Classes/Cliente.php
+++ php-oo-tw2/src/Classes/Cliente.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace App\Classes;
+
+use App\Traits\Imprimir;
 class Cliente extends Pessoa
 {
+    use Imprimir;
+
     public function setId(int $id): void
     {
         $this->id = $id + 1000;
