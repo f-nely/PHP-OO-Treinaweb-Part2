a5a9364c44f8cfc4b08d6f838535a358320024a4
nely de faria <nelly.nf54@gmail.com>
2020-10-06 17:07:53 -0300

    Refatora código

--- php-oo-tw2/src/Classes/Representante.php
+++ php-oo-tw2/src/Classes/Representante.php
@@ -6,7 +6,7 @@ namespace App\Classes;
 
 class Representante extends Vendedor
 {
-    static public function comissao(bool $temBonus): float
+    public static function comissao(bool $temBonus): float
     {
         if ($temBonus) {
             return (parent::$comissao * parent::$bonus) + 1;
